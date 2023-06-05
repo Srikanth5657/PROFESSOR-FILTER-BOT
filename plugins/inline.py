a5d5c7f6dd78e75ed09552fbb79d629ead579b72
Srikanth5657 <130298853+Srikanth5657@users.noreply.github.com>
2023-06-05 13:14:21 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -96,7 +96,7 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('⚡️ Updates Channel ⚡️', url=UPDATES_LINK)
+        InlineKeyboardButton('⚡️ Updates Channel ⚡️', url=UPDATES_LINK),
         InlineKeyboardButton('🔥 Support Group 🔥', url=SUPPORT_LINK)
     ]]
     return InlineKeyboardMarkup(buttons)
